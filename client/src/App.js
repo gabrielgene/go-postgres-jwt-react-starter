9087c11852f6b36580125f6a9585ad4112518f6b
Gabriel Gene <gabrielgene97@gmail.com>
2020-06-08 17:44:53 -0300

    Add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Register from './components/Register';
-import Login from './components/Login';
-import Session from './components/Session';
+import { apiURl } from './api';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 import CustomerList from './pages/CustomerList';
 import CustomerAdd from './pages/CustomerAdd';
+import PaymentAdd from './pages/PaymentAdd';
 import Customer from './pages/Customer';
 import './App.css';
 
 function App() {
+  const [redirect, setRedirect] = React.useState('');
+  React.useEffect(() => {
+    const getUserSession = async () => {
+      const res = await fetch(`${apiURl}/session`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          Authorization: document.cookie,
+        },
+      })
+        .then((res) => res.json())
+        .catch((err) => setRedirect('REDIRECT'));
+
+      if (res && !res.success) {
+        setRedirect('REDIRECT');
+      }
+    };
+    getUserSession();
+  });
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Router>
-        <Route exact path="/" component={CustomerList} />
+        <Route exact path="/" component={Login} />
+        <Route path="/register" component={Register} />
+        {redirect === 'REDIRECT' && <Redirect to="/" />}
+
+        <Route path="/customers" component={CustomerList} />
         <Route exact path="/add/customer" component={CustomerAdd} />
 
         <Route path="/customer/:id" component={Customer} />
-
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/session" component={Session} />
+        <Route exact path="/add/payment" component={PaymentAdd} />
+        <Route exact path="/edit/customer" component={CustomerAdd} />
       </Router>
     </React.Fragment>
   );
