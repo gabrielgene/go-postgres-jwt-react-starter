dc3f9e2f0c879436e9d18fa02633d93af831e727
Gabriel Gene <gabrielgene97@gmail.com>
2020-06-08 09:55:32 -0300

    Add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Session from "./components/Session";
-import "./App.css"
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Register from './components/Register';
+import Login from './components/Login';
+import Session from './components/Session';
+
+import CustomerList from './pages/CustomerList';
+import CustomerAdd from './pages/CustomerAdd';
+import Customer from './pages/Customer';
+import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
+      <CssBaseline />
       <Router>
-           <Route exact path="/" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/session" component={Session} />
+        <Route exact path="/" component={CustomerList} />
+        <Route exact path="/add/customer" component={CustomerAdd} />
+
+        <Route path="/customer/:id" component={Customer} />
+
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/session" component={Session} />
       </Router>
+    </React.Fragment>
   );
 }
 
