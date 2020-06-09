164d54ee188c3114fe39e868a3b644453f62eda1
Gabriel Gene <gabrielgene97@gmail.com>
2020-06-08 22:02:18 -0300

    Fix props use

--- client/src/pages/Customer/openMap.js
+++ client/src/pages/Customer/openMap.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 
-const OpenMap = (position) => (
-  <Map center={position} zoom={12} width={300} height={200}>
-    <Marker anchor={position} payload={1} />
-  </Map>
-);
+const OpenMap = ({ position }) => {
+  return (
+    <Map center={position} zoom={12} width={300} height={200}>
+      <Marker anchor={position} payload={1} />
+    </Map>
+  );
+};
 
 export default OpenMap;
